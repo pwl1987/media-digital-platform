f1c9d106c94bee1242494230a9972d5c237a01f7
pwl1987 <13573975690@163.com>
2026-08-29 18:16:29 +0800

    fix(opera): make work category filters functional

--- apps/miniapp-opera/pages/works/works.js
+++ apps/miniapp-opera/pages/works/works.js
@@ -1,18 +1,30 @@
 const { works } = require('../../utils/mock');
 
 Page({
-  data: { works: [], activeTag: '全部' },
+  data: { allWorks: works, works, activeTag: '全部' },
   filterTags: ['全部', '传统戏曲', '现代小戏', '红色题材', '地方戏', '精品剧目'],
 
   onLoad() {
-    this.setData({ works });
+    this.filterWorks('全部');
   },
 
   selectTag(event) {
-    this.setData({ activeTag: event.currentTarget.dataset.tag });
+    this.filterWorks(event.currentTarget.dataset.tag);
+  },
+
+  filterWorks(tag) {
+    const filtered = tag === '全部'
+      ? this.data.allWorks
+      : this.data.allWorks.filter((item) => item.tag === tag || (tag === '精品剧目' && item.tag === '精品剧目'));
+    this.setData({ activeTag: tag, works: filtered });
   },
 
   openWork(event) {
     wx.navigateTo({ url: `/pages/work-detail/work-detail?id=${event.currentTarget.dataset.id}` });
+  },
+
+  onPullDownRefresh() {
+    this.filterWorks(this.data.activeTag);
+    wx.stopPullDownRefresh();
   }
 });
