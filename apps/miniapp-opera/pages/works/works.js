3f8290617b2d838db860068feb53717ed5cfd13f
pwl1987 <13573975690@163.com>
2026-08-29 18:06:33 +0800

    feat: connect Opera work list to content filters

--- apps/miniapp-opera/pages/works/works.js
+++ apps/miniapp-opera/pages/works/works.js
@@ -1,12 +1,18 @@
 const { works } = require('../../utils/mock');
 
 Page({
-  data: { works: [] },
+  data: { works: [], activeTag: '全部' },
+  filterTags: ['全部', '传统戏曲', '现代小戏', '红色题材', '地方戏', '精品剧目'],
+
   onLoad() {
     this.setData({ works });
   },
+
+  selectTag(event) {
+    this.setData({ activeTag: event.currentTarget.dataset.tag });
+  },
+
   openWork(event) {
-    const { id } = event.currentTarget.dataset;
-    wx.navigateTo({ url: `/pages/work-detail/work-detail?id=${id}` });
+    wx.navigateTo({ url: `/pages/work-detail/work-detail?id=${event.currentTarget.dataset.id}` });
   }
 });
