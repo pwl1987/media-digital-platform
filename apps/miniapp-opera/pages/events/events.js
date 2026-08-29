0fbdb9d3a8417ac77e521c1af9a9201204e4e748
pwl1987 <13573975690@163.com>
2026-08-29 18:03:29 +0800

    feat(opera): wire exhibition list

--- apps/miniapp-opera/pages/events/events.js
+++ apps/miniapp-opera/pages/events/events.js
@@ -1,1 +1,11 @@
-Page({});
+const { events } = require('../../utils/mock');
+
+Page({
+  data: { events: [] },
+  onLoad() {
+    this.setData({ events });
+  },
+  openEvent(event) {
+    wx.navigateTo({ url: `/pages/event-detail/event-detail?id=${event.currentTarget.dataset.id}` });
+  }
+});
