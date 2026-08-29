503236d6c5693256541ce26aa4b5e884158dc6ba
pwl1987 <13573975690@163.com>
2026-08-29 18:02:32 +0800

    feat(opera): wire works list to mock catalog

--- apps/miniapp-opera/pages/works/works.js
+++ apps/miniapp-opera/pages/works/works.js
@@ -1,1 +1,12 @@
-Page({});
+const { works } = require('../../utils/mock');
+
+Page({
+  data: { works: [] },
+  onLoad() {
+    this.setData({ works });
+  },
+  openWork(event) {
+    const { id } = event.currentTarget.dataset;
+    wx.navigateTo({ url: `/pages/work-detail/work-detail?id=${id}` });
+  }
+});
