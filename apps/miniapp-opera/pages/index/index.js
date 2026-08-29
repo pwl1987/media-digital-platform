6fdf064363efeab2f40cd8c64127f31143404822
pwl1987 <13573975690@163.com>
2026-08-29 17:16:40 +0800

    feat: wire Opera home navigation

---
+++ apps/miniapp-opera/pages/index/index.js
@@ -0,0 +1,5 @@
+Page({
+  goWorks() { wx.switchTab({ url: '/pages/works/works' }); },
+  goEvents() { wx.switchTab({ url: '/pages/events/events' }); },
+  goLive() { wx.switchTab({ url: '/pages/live/live' }); }
+});
