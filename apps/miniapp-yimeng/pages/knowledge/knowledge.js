7205646f49549ca7d6440163b70934c5f23f966c
pwl1987 <13573975690@163.com>
2026-08-29 17:24:45 +0800

    feat: wire Yimeng knowledge mock data

--- apps/miniapp-yimeng/pages/knowledge/knowledge.js
+++ apps/miniapp-yimeng/pages/knowledge/knowledge.js
@@ -1,1 +1,7 @@
-Page({});
+const { knowledgeItems } = require('../../utils/mock');
+
+Page({
+  data: { items: knowledgeItems },
+  goSearch() { wx.showToast({ title: '搜索页即将接入', icon: 'none' }); },
+  openItem(e) { wx.navigateTo({ url: e.currentTarget.dataset.path }); }
+});
